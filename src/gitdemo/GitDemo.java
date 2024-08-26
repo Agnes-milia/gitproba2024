711dbfd56eef2ea8329f3a3c4a13cdd01d9b560d
Agnes-milia <ratipotti@gmail.com>
2024-08-26 15:19:43 +0200

    aaa

--- src/gitdemo/GitDemo.java
+++ src/gitdemo/GitDemo.java
@@ -4,11 +4,9 @@ package gitdemo;
 
 public class GitDemo {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("vvv");
     }
     
 }
